7ea0a3e81f6e9f36ae27e9bd4693abd74e47f3b5
LOctavio <luis_ram1997@hotmail.com>
2021-09-16 16:06:27 -0500

    Get list from API and store values in reduxe store

--- src/components/homepage/homepage.js
+++ src/components/homepage/homepage.js
@@ -1,3 +1,32 @@
-const HomePage = () => (<h1>Home Page</h1>);
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import axios from 'axios';
+import { getList } from '../../redux/homepage/homepage';
+
+const HomePage = () => {
+  const dispatch = useDispatch();
+
+  const CountriesList = () => {
+    axios.get('https://api.covid19tracking.narrativa.com/api/2021-08-01')
+      .then((response) => {
+        const list = [];
+        let [countries] = [...Object.values(response.data.dates)];
+        countries = countries.countries;
+        Object.entries(countries).forEach((element) => {
+          list.push({
+            country: element[0],
+            confirmed_cases: element[1].today_confirmed,
+          });
+        });
+        dispatch(getList(list));
+      });
+  };
+
+  useEffect(() => {
+    CountriesList();
+  }, []);
+
+  return (<h1>Home Page</h1>);
+};
 
 export default HomePage;
