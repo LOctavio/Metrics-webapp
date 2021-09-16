592a83d7036e65242358be38a698b72af9e2cb78
LOctavio <luis_ram1997@hotmail.com>
2021-09-16 15:45:55 -0500

    Add provider

--- src/index.js
+++ src/index.js
@@ -2,21 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './index.css';
+import { Provider } from 'react-redux';
 import Details from './components/details/details';
 import HomePage from './components/homepage/homepage';
+import store from './redux/configStore';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <HomePage />
-        </Route>
-        <Route path="/details">
-          <Details />
-        </Route>
-      </Switch>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route path="/details">
+            <Details />
+          </Route>
+        </Switch>
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
