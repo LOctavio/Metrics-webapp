ea9144488e3858808ed77fc5d5b3db7043ec4299
LOctavio <luis_ram1997@hotmail.com>
2021-09-16 02:55:09 -0500

    Add routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './index.css';
+import Details from './components/details/details';
+import HomePage from './components/homepage/homepage';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>hi</h1>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route path="/details">
+          <Details />
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
