2ba4e843fca5447a7b2c346112eed27b731b493a
LOctavio <luis_ram1997@hotmail.com>
2021-09-17 10:53:24 -0500

    Fix reducer to just store the list once

--- src/redux/details/details.js
+++ src/redux/details/details.js
@@ -10,7 +10,7 @@ export const getList = (list) => ({
 const detailsReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_LIST:
-      return [...state, ...action.payload];
+      return action.payload;
     default:
       return state;
   }
