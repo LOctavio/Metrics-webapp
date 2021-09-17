fcbd03ab099f136c677a13136797e10fc2bb5f23
LOctavio <luis_ram1997@hotmail.com>
2021-09-17 15:46:26 -0500

    Add styles for header

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,30 @@
 import PropTypes from 'prop-types';
+import { BsFillMicFill, BsFillGearFill, BsChevronLeft } from 'react-icons/bs';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Header = (props) => {
   const { title } = props;
   return (
-    <header className="header">
-      <button type="button">
-        hi
-      </button>
-      <h2>{title}</h2>
-    </header>
+    <Container className="header">
+      <Row className="justify-content-md-center">
+        <Col className="d-flex align-items-center">
+          <button type="button" className="back-button">
+            <BsChevronLeft />
+          </button>
+        </Col>
+        <Col xs={6} className="title">
+          <span>{title}</span>
+        </Col>
+        <Col className="d-flex justify-content-end align-items-center icons">
+          <Col>
+            <BsFillMicFill />
+          </Col>
+          <Col>
+            <BsFillGearFill />
+          </Col>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
